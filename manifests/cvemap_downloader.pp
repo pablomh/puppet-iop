b8eaf71226ac58af0d6728701d8f07ee9eb33d69
Eric D. Helms <ericdhelms@gmail.com>
2025-09-03 08:55:10 -0400

    Trigger a reposcan when cvemap is downloaded
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- manifests/cvemap_downloader.pp
+++ manifests/cvemap_downloader.pp
@@ -10,11 +10,17 @@
 #
 # $timer_interval:: Interval between timer executions (default: 24h)
 #
+# $enable_initial_download:: Whether to enable one-time initial cvemap download
+#
 class iop::cvemap_downloader (
   Enum['present', 'absent'] $ensure = 'present',
   String $base_url = 'https://security.access.redhat.com/data/meta/v1/cvemap.xml',
   String $timer_interval = '24h',
+  Boolean $enable_initial_download = true,
 ) {
+  include iop::core_gateway
+  include iop::service_vmaas
+
   $script_path = '/usr/local/bin/iop-cvemap-download.sh'
   $basedir = '/var/www/html/pub'
   $relative_path = 'iop/data/meta/v1/cvemap.xml'
@@ -32,8 +38,8 @@ class iop::cvemap_downloader (
     ensure        => $ensure,
     unit_entry    => {
       'Description' => 'Manages cvemap.xml for IoP Vulnerability',
-      'After'       => 'network-online.target',
-      'Wants'       => 'network-online.target',
+      'After'       => ['network-online.target', 'iop-core-gateway.service'],
+      'Wants'       => ['network-online.target', 'iop-core-gateway.service'],
     },
     service_entry => {
       'Type'      => 'oneshot',
@@ -44,7 +50,10 @@ class iop::cvemap_downloader (
     install_entry => {},
     enable        => false,
     active        => false,
-    require       => File[$script_path],
+    require       => [
+      File[$script_path],
+      Class['iop::core_gateway'],
+    ],
   }
 
   $unit_enable = $ensure ? { 'present' => true, 'absent' => false }
@@ -65,5 +74,19 @@ class iop::cvemap_downloader (
     active  => $unit_active,
     content => file('iop/iop-cvemap-download.path'),
     require => Systemd::Manage_unit['iop-cvemap-download.service'],
+  }
+
+  # Initial download to ensure cvemap.xml exists
+  if $enable_initial_download {
+    exec { 'iop-cvemap-initial-download':
+      command => "${script_path} '${base_url}' '${full_path}'",
+      creates => $full_path,
+      path    => ['/usr/bin', '/bin', '/usr/local/bin'],
+      require => [
+        File[$script_path],
+        Class['iop::core_gateway'],
+        Service['iop-service-vmaas-reposcan'],
+      ],
+    }
   }
 }
--- spec/classes/cvemap_downloader_spec.rb
+++ spec/classes/cvemap_downloader_spec.rb
@@ -7,10 +7,12 @@ describe 'iop::cvemap_downloader' do
 
       context 'with default parameters' do
         it { should compile.with_all_deps }
+        it { should contain_class('iop::core_gateway') }
         it { should contain_file('/usr/local/bin/iop-cvemap-download.sh') }
         it { should contain_systemd__manage_unit('iop-cvemap-download.service') }
         it { should contain_systemd__timer('iop-cvemap-download.timer') }
         it { should contain_systemd__unit_file('iop-cvemap-download.path') }
+        it { should contain_exec('iop-cvemap-initial-download') }
       end
 
       context 'with ensure => absent' do
@@ -28,6 +30,7 @@ describe 'iop::cvemap_downloader' do
         
         it { should compile.with_all_deps }
         it { should contain_systemd__manage_unit('iop-cvemap-download.service') }
+        it { should contain_exec('iop-cvemap-initial-download') }
       end
 
       context 'with custom timer_interval' do
@@ -35,6 +38,31 @@ describe 'iop::cvemap_downloader' do
         
         it { should compile.with_all_deps }
         it { should contain_systemd__timer('iop-cvemap-download.timer') }
+      end
+
+      context 'with enable_initial_download => false' do
+        let(:params) { { enable_initial_download: false } }
+        
+        it { should compile.with_all_deps }
+        it { should_not contain_exec('iop-cvemap-initial-download') }
+      end
+
+      context 'gateway dependency verification' do
+        it { should compile.with_all_deps }
+        
+        describe 'systemd unit dependencies' do
+          let(:unit_entry) { catalogue.resource('Systemd::Manage_unit', 'iop-cvemap-download.service')[:unit_entry] }
+          
+          it 'has correct After dependencies' do
+            expect(unit_entry['After']).to eq(['network-online.target', 'iop-core-gateway.service'])
+          end
+          
+          it 'has correct Wants dependencies' do
+            expect(unit_entry['Wants']).to eq(['network-online.target', 'iop-core-gateway.service'])
+          end
+        end
+        
+        it { should contain_systemd__manage_unit('iop-cvemap-download.service').that_requires('Class[iop::core_gateway]') }
       end
     end
   end
